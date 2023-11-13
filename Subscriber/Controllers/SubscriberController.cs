8f08d61672302786054b2f5603b6ab1b52fb97ba
Damith Wanninayake <damith.uwu@gmail.com>
2023-11-13 13:42:50 +0530

    initial files are created

---
+++ Subscriber/Controllers/SubscriberController.cs
@@ -0,0 +1,26 @@
+﻿using Dapr;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Subscriber.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubscriberController : ControllerBase
+    {
+        ILogger<SubscriberController> logger;
+
+        public SubscriberController(ILogger<SubscriberController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [Topic("myapp-pubsub", "test-dapr-topic")]
+        [HttpPost("subscriber")]
+        public IActionResult Post([FromBody] string value)
+        {
+            logger.LogInformation(value);
+            return Ok(value);
+        }
+    }
+}
